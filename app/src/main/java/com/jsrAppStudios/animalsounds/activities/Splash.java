91849924d1e424cbbac9de0e2e30b6fef87f7d5a
vinoddirishala <dirishalavinodkumar@gmail.com>
2022-04-06 14:33:00 +0530

    working on dashboard

--- app/src/main/java/com/jsrAppStudios/animalsounds/activities/Splash.java
+++ app/src/main/java/com/jsrAppStudios/animalsounds/activities/Splash.java
@@ -14,7 +14,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-       // runApp();
+        runApp();
     }
 
     public void runApp(){

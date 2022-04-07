66c6edd1a0dbae57ce5f222041c804eed50b4a60
vinoddirishala <dirishalavinodkumar@gmail.com>
2022-04-07 13:28:53 +0530

    settings screen completed...!!

--- app/src/main/java/com/jsrAppStudios/animalsounds/activities/SettingsActivity.java
+++ app/src/main/java/com/jsrAppStudios/animalsounds/activities/SettingsActivity.java
@@ -2,12 +2,15 @@ package com.jsrAppStudios.animalsounds.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.jsrAppStudios.animalsounds.BuildConfig;
 import com.jsrAppStudios.animalsounds.R;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -39,16 +42,50 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 finish();
                 break;
             case R.id.shareUS:
-                startActivity(new Intent());
+                shareApp();
                 break;
             case R.id.rateUS:
-
+                rateApp();
                 break;
             case R.id.feedback:
+                composeEmail("jsrappstudios@gmail.com\n","Feedback for Animal Tones & Wallpapers App");
                 break;
             case R.id.moreApps:
-
+                 moreApp();
                 break;
+        }
+    }
+
+    public void composeEmail(String email, String subject) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + email));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Feedback for Animal Tones & Wallpapers App \n\n\n");
+        startActivity(Intent.createChooser(emailIntent, "Chooser Title"));
+    }
+
+    public void rateApp(){
+        try{
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+getPackageName())));
+        }
+        catch (ActivityNotFoundException e){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id="+getPackageName())));
+        }
+    }
+
+    public void shareApp(){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT,
+                "Hey check out my app at: https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+    public void moreApp(){
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:7446766861106650979")));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=7446766861106650979")));
         }
     }
 }
